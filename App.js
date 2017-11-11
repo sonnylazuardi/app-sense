4372e9a1508bb5412828a7ea8804cb10c7c8afa2
Sonny Lazuardi Hermawan <sonnylazuardi@gmail.com>
2017-11-12 02:36:31 +0800

    Add experiment with jsx parser

--- App.js
+++ App.js
@@ -4,34 +4,59 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Switch, Button } from "react-native";
+import JsxParser from "react-jsx-parser";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 export default class App extends Component<{}> {
+  state = {
+    counter: 1,
+    htmlAmpas: `<View><Text>Apakah ampas?</Text><Text>Tes lagi dong?</Text><Switch value={true}/><Button onPress={'buttonPressed'} title="TESTING" /></View>`,
+    functionAmpas: `
+      alert('1');
+      this.setState({
+        counter: 3,
+      });
+      alert('2');
+    `
+  };
+  onChangeHTML = () => {
+    this.setState({
+      htmlAmpas: `<Text>Saya gak ampas</Text>`
+    });
+  };
+  evalInContext(js, context) {
+    return function() {
+      return eval(js);
+    }.call(context);
+  }
   render() {
+    const { counter } = this.state;
+    const buttonPressed = () => {
+      this.evalInContext(this.state.functionAmpas, this);
+    };
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text>{counter}</Text>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <Button title="Ganti HTML" onPress={this.onChangeHTML} />
+        <JsxParser
+          // bindings={{ onPress: buttonPressed }}
+          callbacks={{
+            buttonPressed
+          }}
+          components={{ Text, Switch, View, Button }}
+          jsx={this.state.htmlAmpas}
+        />
       </View>
     );
   }
@@ -40,18 +65,18 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
