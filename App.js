c2680b64d3cb34eade69cbb0ff22cd8fc0af446a
Sonny Lazuardi Hermawan <sonnylazuardi@gmail.com>
2017-11-13 16:42:24 +0800

    Add ui editor

--- App.js
+++ App.js
@@ -5,28 +5,154 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Switch, Button } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Switch,
+  Button,
+  TouchableOpacity,
+  Animated,
+  Image,
+  Easing
+} from "react-native";
 import JsxParser from "./lib/jsxParser/components/JsxParser";
+import SortableList from "react-native-sortable-list";
+import Row from "./src/components/row";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+const data = {
+  state: [
+    {
+      name: "isAmpas",
+      value: false
+    },
+    {
+      name: "ampasText",
+      value: "Teks ini tidak ampas"
+    }
+  ],
+  components: [
+    {
+      type: "Switch",
+      value: {
+        type: "value",
+        value: false
+      },
+      text: {
+        type: "value",
+        value: "Ubah jadi ampas"
+      },
+      color: {
+        type: "value",
+        value: "#333"
+      },
+      fontSize: {
+        type: "value",
+        value: 15
+      }
+    },
+    {
+      type: "Text",
+      text: {
+        type: "value",
+        value: "Teks lagi nih"
+      },
+      color: {
+        type: "value",
+        value: "#2ecc71"
+      },
+      fontSize: {
+        type: "value",
+        value: 16
+      }
+    },
+    {
+      type: "Button",
+      title: {
+        type: "value",
+        value: "SUBMIT"
+      },
+      color: {
+        type: "value",
+        value: "#2ecc71"
+      }
+    },
+    {
+      type: "Text",
+      text: {
+        type: "value",
+        value: "Teks ini tidak ampas"
+      },
+      color: {
+        type: "value",
+        value: "#333"
+      },
+      fontSize: {
+        type: "value",
+        value: 15
+      }
+    }
+  ]
+};
 
 export default class App extends Component<{}> {
   state = {
+    state: data.state,
+    components: data.components,
     counter: 1,
     htmlAmpas: `<View><Text>Apakah ampas?</Text><Text>Tes lagi dong?</Text><Switch value={true}/><Button onPress={'buttonPressed'} title="TESTING" /></View>`,
-    functionAmpas: `
-      alert('1');
-      this.setState({
-        counter: 3,
-      });
-      alert('2');
-    `
+    functionAmpas: `alert('1');this.setState({counter: 3,});alert('2');`
   };
+  _nextOrder = [];
+  _componentToJSX(component) {
+    switch (component.type) {
+      case "Text":
+        return (
+          <Text
+            style={{
+              fontSize: component.fontSize.value,
+              color: component.color.value
+            }}
+          >
+            {component.text.value}
+          </Text>
+        );
+        break;
+      case "Switch":
+        return (
+          <View style={{ flexDirection: "row" }}>
+            <Switch value={component.value.value} />
+            <Text
+              style={{
+                fontSize: component.fontSize.value,
+                color: component.color.value
+              }}
+            >
+              {component.text.value}
+            </Text>
+          </View>
+        );
+      case "Button":
+        return (
+          <Button
+            title={component.title.value}
+            color={component.color.value}
+            onPress={() => alert("test")}
+          />
+        );
+    }
+  }
+  _formatComponents(components) {
+    let result = {};
+    components.forEach((component, i) => {
+      result[i] = {
+        ...component,
+        component: this._componentToJSX(component)
+      };
+    });
+    return result;
+  }
   onChangeHTML = () => {
     this.setState({
       htmlAmpas: `<Text>Saya gak ampas</Text>`
@@ -37,6 +163,81 @@ export default class App extends Component<{}> {
       return eval(js);
     }.call(context);
   }
+  _renderRow = ({ data, active }) => {
+    return <Row data={data} active={active} />;
+  };
+  _reorderComponents(components, nextOrder) {
+    return nextOrder.map(orderId => {
+      const current = components[orderId];
+      return current;
+    });
+  }
+  _addButton() {
+    this.setState({
+      components: [
+        ...this.state.components,
+        {
+          type: "Button",
+          title: {
+            type: "value",
+            value: "SUBMIT"
+          },
+          color: {
+            type: "value",
+            value: "#2ecc71"
+          }
+        }
+      ]
+    });
+  }
+  _addSwitch() {
+    this.setState({
+      components: [
+        ...this.state.components,
+        {
+          type: "Switch",
+          value: {
+            type: "value",
+            value: false
+          },
+          text: {
+            type: "value",
+            value: "Switch"
+          },
+          color: {
+            type: "value",
+            value: "#333"
+          },
+          fontSize: {
+            type: "value",
+            value: 15
+          }
+        }
+      ]
+    });
+  }
+  _addText() {
+    this.setState({
+      components: [
+        ...this.state.components,
+        {
+          type: "Text",
+          text: {
+            type: "value",
+            value: "Teks baru"
+          },
+          color: {
+            type: "value",
+            value: "#333"
+          },
+          fontSize: {
+            type: "value",
+            value: 15
+          }
+        }
+      ]
+    });
+  }
   render() {
     const { counter } = this.state;
     const buttonPressed = () => {
@@ -44,7 +245,7 @@ export default class App extends Component<{}> {
     };
     return (
       <View style={styles.container}>
-        <Text>{counter}</Text>
+        {/* <Text>{counter}</Text>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
@@ -56,7 +257,50 @@ export default class App extends Component<{}> {
           }}
           components={{ Text, Switch, View, Button }}
           jsx={this.state.htmlAmpas}
-        />
+        /> */}
+        <View style={styles.toolbar}>
+          <TouchableOpacity
+            style={styles.toolButton}
+            onPress={() => this._addText()}
+          >
+            <Text style={styles.toolButtonText}>Text</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.toolButton}
+            onPress={() => this._addButton()}
+          >
+            <Text style={styles.toolButtonText}>Button</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.toolButton}
+            onPress={() => this._addSwitch()}
+          >
+            <Text style={styles.toolButtonText}>Switch</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.canvas}>
+          <SortableList
+            style={styles.list}
+            onChangeOrder={nextOrder => {
+              this._nextOrder = nextOrder;
+            }}
+            onReleaseRow={key => {
+              setTimeout(() => {
+                this.setState({
+                  components: this._reorderComponents(
+                    this.state.components,
+                    this._nextOrder
+                  )
+                });
+              });
+            }}
+            onPressRow={key => {}}
+            onActivateRow={key => {}}
+            contentContainerStyle={styles.contentContainer}
+            data={this._formatComponents(this.state.components)}
+            renderRow={this._renderRow}
+          />
+        </View>
       </View>
     );
   }
@@ -65,18 +309,25 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#FFF",
+    flexDirection: "row"
+  },
+  toolbar: {
+    flex: 1
+  },
+  list: {
+    flex: 1
+  },
+  contentContainer: {
+    flex: 1
+  },
+  canvas: {
+    flex: 2,
+    backgroundColor: "#eee"
+  },
+  toolButton: {
+    height: 60,
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+    justifyContent: "center"
   }
 });
---
+++ src/components/row.js
@@ -0,0 +1,109 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Switch,
+  Button,
+  TouchableOpacity,
+  Animated,
+  Image,
+  Easing
+} from "react-native";
+
+export default class Row extends Component {
+  constructor(props) {
+    super(props);
+
+    this._active = new Animated.Value(0);
+
+    this._style = {
+      ...Platform.select({
+        ios: {
+          transform: [
+            {
+              scale: this._active.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 1.1]
+              })
+            }
+          ],
+          shadowRadius: this._active.interpolate({
+            inputRange: [0, 1],
+            outputRange: [2, 10]
+          })
+        },
+
+        android: {
+          transform: [
+            {
+              scale: this._active.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 1.07]
+              })
+            }
+          ],
+          elevation: this._active.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 3]
+          })
+        }
+      })
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.active !== nextProps.active) {
+      Animated.timing(this._active, {
+        duration: 300,
+        easing: Easing.bounce,
+        toValue: Number(nextProps.active)
+      }).start();
+    }
+  }
+
+  render() {
+    const { data, active } = this.props;
+
+    return (
+      <Animated.View style={[styles.row, this._style]}>
+        {data.component}
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "transparent",
+    padding: 10,
+    height: 60,
+    flex: 1,
+    marginTop: 3,
+    marginBottom: 3,
+    borderRadius: 3,
+
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgba(0,0,0,0.2)",
+        shadowOpacity: 1,
+        shadowOffset: { height: 2, width: 2 },
+        shadowRadius: 2
+      },
+
+      android: {
+        elevation: 0,
+        marginHorizontal: 10
+      }
+    })
+  }
+});
